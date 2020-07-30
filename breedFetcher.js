80b755079f6a9db47f3bff8c993061b43ed90383
Em Urdaneta <em.urdaneta94@gmail.com>
2020-07-30 18:41:12 +0000

    created the function to access the api with user input

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,27 @@
 const request = require('request');
+const fs = require('fs');
+const { Console } = require('console');
 
-request('https://api.thecatapi.com/v1/breeds/search.', (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
+const breed = process.argv.slice(2).join('');
+// const apiCall = ;
+//red through input
+//console.log(breed);
+request('https://api.hecatapi.com/v1/breeds/search?q='+breed, (error, response, body) => {
+
+  //if something is wrong in general
+  if (error.statusCode === undefined) {
+    console.log(error['errno']);
+  } else{
+    const data = JSON.parse(body);
+    if ( data[0] === undefined || data[0].length === 0) {
+      console.log("Breed not found");
+    } else {
+      console.log(data[0]['description']);
+    }
+  }
+ 
+ 
+  //check both conditions because not having the breed will return null but having a syntax error in the url string will return an error value so I'm checking the response
+
 
 });
