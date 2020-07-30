dc6cafaafad867be713a2768e2c55c5fe12fa830
Em Urdaneta <em.urdaneta94@gmail.com>
2020-07-30 19:40:09 +0000

    refactored breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,20 @@
 const request = require('request');
-const fs = require('fs');
-const { Console } = require('console');
 
-const breed = process.argv.slice(2).join('');
-// const apiCall = ;
-//red through input
-//console.log(breed);
-request('https://api.hecatapi.com/v1/breeds/search?q='+breed, (error, response, body) => {
-
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
   //if something is wrong in general
-  if (error.statusCode === undefined) {
-    console.log(error['errno']);
-  } else{
+  if (error !== null) {
+    callback(error,null);
+  } else {
     const data = JSON.parse(body);
-    if ( data[0] === undefined || data[0].length === 0) {
-      console.log("Breed not found");
+    if (data[0] === undefined || data[0].length === 0) {
+      callback(error,null);
     } else {
-      console.log(data[0]['description']);
+      callback(null,data[0]['description']);
     }
   }
- 
- 
-  //check both conditions because not having the breed will return null but having a syntax error in the url string will return an error value so I'm checking the response
-
-
 });
+};
+
+module.exports = { fetchBreedDescription };
+
